ffa4abad6fce113d0a64ba3b08e5bd187301bb61
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-03-23 14:42:02 +0700

    project form validator #1

---
+++ fromValidator/script.js
@@ -0,0 +1,4 @@
+const input = document.querySelector("form");
+const errorEmail = document.getElementById("error-email");
+const errorPass = document.getElementById("error-password");
+const errorTelp = document.getElementById("error-telp");
