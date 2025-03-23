033db5f9e4da91c66f25ecd6c65dd8ca8ead9b18
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-03-23 16:49:46 +0700

    project form validator #2

--- fromValidator/script.js
+++ fromValidator/script.js
@@ -1,4 +1,33 @@
-const input = document.querySelector("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const telp = document.getElementById("telp");
+const submit = document.querySelector("#submit");
 const errorEmail = document.getElementById("error-email");
 const errorPass = document.getElementById("error-password");
 const errorTelp = document.getElementById("error-telp");
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  cekEmail(email.value);
+});
+
+function cekEmail(value) {
+  if (value === "") {
+    errorEmail.textContent = "Email tidak boleh kosong!";
+    email.classList.add("error");
+    email.classList.remove("success");
+    errorEmail.style.color = "red";
+  } else if (!regexEmail.test(value)) {
+    errorEmail.textContent = "Format email tidak valid";
+    email.classList.add("error");
+    email.classList.remove("success");
+    errorEmail.style.color = "red";
+  } else {
+    errorEmail.textContent = "Format email valid";
+    email.classList.add("success");
+    email.classList.remove("error");
+    errorEmail.style.color = "green";
+  }
+  errorEmail.style.display = "block";
+}
