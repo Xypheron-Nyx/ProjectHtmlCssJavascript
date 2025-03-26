b84ce3573567808af02fa511a9e09a7ce0842ab0
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-03-26 22:56:09 +0700

    project quizz app #4

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -23,16 +23,13 @@ let currentIndex = 0;
 // kumpulan pertanyaan sesuai level, sama aja kayak filteredQuestions tapi ini untuk global
 let currentQuestions = [];
 
+// total benar
+let totallyCorrect = [];
+let goodAnswer = [];
+
 // Start button event = untuk masuk ke pemilihan mode kesulitan
 startBtn.addEventListener("click", function () {
   showDifficultySelection();
-});
-
-// Difficulty button event = untuk masuk ke quiz
-diffButton.addEventListener("click", function (e) {
-  let mode = e.target.dataset.level;
-  let filteredQuestions = getQuestionsByLevel(mode);
-  startQuiz(filteredQuestions);
 });
 
 // Function untuk menampilkan pilihan kesulitan
@@ -43,6 +40,13 @@ function showDifficultySelection() {
     diffButton.classList.remove("opacity-0", "translate-y-10", "blur-md");
   });
 }
+
+// Difficulty button event = untuk masuk ke quiz
+diffButton.addEventListener("click", function (e) {
+  let mode = e.target.dataset.level;
+  let filteredQuestions = getQuestionsByLevel(mode);
+  startQuiz(filteredQuestions);
+});
 
 // filter mode
 function getQuestionsByLevel(level) {
@@ -55,22 +59,12 @@ function startQuiz(filteredQuestions) {
   currentIndex = 0;
   // simpan pertanyaan ke currentQuestions
   currentQuestions = filteredQuestions;
-  console.log(currentQuestions);
   // Acak pertanyaan
   shuffle(currentQuestions);
   // Acak pilihan di setiap pertanyaan
   currentQuestions.forEach((currentQuestion) => shuffle(currentQuestion.pilihan));
   // tampilkan soal pertama
   showQuestion(currentQuestions[currentIndex]); // Tampilkan soal pertama
-  console.log(currentQuestions[currentIndex]);
-}
-
-// function shuffle
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
 }
 
 // function startAnimation
@@ -82,29 +76,54 @@ function startAnimation() {
   }, 10);
 }
 
+// function shuffle
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 // function showQuestion
 function showQuestion(currentQuestion) {
   levelEl.textContent = currentQuestion.level;
   questionEl.textContent = currentQuestion.soal;
-  optionsEl.forEach((optionBtn, index) => {
-    optionBtn.textContent = currentQuestion.pilihan[index];
-
-    optionBtn.addEventListener("click", function () {
-      nextQuestion(); // Tidak perlu kasih parameter lagi
-    });
+  optionsEl.forEach((btn, index) => {
+    btn.textContent = currentQuestion.pilihan[index]; // Perbarui teks tombol
   });
 
-  // Update currentIndex soal
+  // Update nomor soal
   currentQuest.textContent = currentIndex + 1;
+  totalQuest.textContent = currentQuestions.length;
 }
 
-// pindah soal
+// function tangkapPilihan
+optionsEl.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    totallyCorrect.push(e.target.textContent);
+    console.log(totallyCorrect);
+    nextQuestion();
+  });
+});
+
+// function pindah soal
 function nextQuestion() {
   if (currentIndex < currentQuestions.length - 1) {
     currentIndex++; // Naik ke soal berikutnya
     showQuestion(currentQuestions[currentIndex]); // Tampilkan soal baru
-    console.log(currentIndex);
   } else {
     alert("Kuis selesai!");
+    checkAnswer();
+    optionsEl.forEach((btn) => btn.setAttribute("disabled", "true"));
+    console.log(goodAnswer);
   }
+}
+
+// function cek jawaban
+function checkAnswer() {
+  totallyCorrect.forEach((g, i) => {
+    if (g === currentQuestions[i].jawaban) {
+      goodAnswer.push(g);
+    }
+  });
 }
